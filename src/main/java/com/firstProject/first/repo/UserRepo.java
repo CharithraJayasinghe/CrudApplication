1db1f08fbe763dc5bc5beeebbe8746617b94d2f0
Charithra Jayasinghe <charithraseneth3@gmail.com>
2023-03-11 23:23:06 +0530

    different quaries

--- src/main/java/com/firstProject/first/repo/UserRepo.java
+++ src/main/java/com/firstProject/first/repo/UserRepo.java
@@ -2,6 +2,12 @@ package com.firstProject.first.repo;
 
 import com.firstProject.first.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
+
+    @Query(value = "SELECT * FROM USER WHERE ID = ?1",nativeQuery = true)          // @Query annotation takes precedence over named queries
+    User getUserByUserID(String userId);
+    @Query(value = "SELECT * FROM USER WHERE ID = ?1 AND ADDRESS = ?2", nativeQuery = true)
+    User getUserByUserIDAndAddress(String userId, String address);
 }
--- src/main/java/com/firstProject/first/service/UserService.java
+++ src/main/java/com/firstProject/first/service/UserService.java
@@ -36,5 +36,14 @@ public class UserService {
         userRepo.delete(modelMapper.map(userDTO,User.class));
         return true;
     }
+    //user id > user details
+    //select * from user where id = 2
+    public UserDTO getUserByUserID(String userID){
+        User user=userRepo.getUserByUserID(userID);
+        return modelMapper.map(user,UserDTO.class);
+    }
+
+
+
 
 }
